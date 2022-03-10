27c9e33a98f2e943f63f8062095a65fa6f0ca734
GorrettyOkumbe <okumbegorretty4@gmail.com>
2022-03-10 06:30:23 -0800

    My 1st c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/**
+/*
  * main - prints the phrase "Programming is like building a multillingual puzzle
  * Return: 0 if executed properly ,non-zero otherwise
 */ 
@@ -8,7 +8,7 @@
 int main(void)
 
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	
 	return (0);	
 }
