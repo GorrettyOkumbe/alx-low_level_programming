b23f113647381abd20460231f0961c154f75b0b2
GorrettyOkumbe <okumbegorretty4@gmail.com>
2022-03-15 12:04:50 -0700

    combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,16 +8,19 @@ int main(void)
 {
 	int n, m;
 
-	for (n = 79; n <= 89; n++)
+	for (n = 48; n <= 56; n++)
 	{
-		for (m = 79; m <= 89; m++)
+		for (m = 49; m <= 57; m++)
 		{
-			putchar(n);
-			putchar(m);
-			if (n != 88 || m != 88)
+			if (m > n)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
