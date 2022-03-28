f0901c3d4d85b827435af32b0d747c8a77a2f950
GorrettyOkumbe <okumbegorretty4@gmail.com>
2022-03-28 16:14:40 -0700

    character checker

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string.
+ * @s: pointer to a character
+ * @c: character to be located
+ *Return:pointer to c or NULL if c is not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
+	return (0);
+}
